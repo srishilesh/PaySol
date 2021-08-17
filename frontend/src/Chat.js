b8a67bc0c763047696426cbe49e85e3408e67ee2
Srishilesh P S <32015336+srishilesh@users.noreply.github.com>
2021-08-17 23:37:45 +0530

    Feat: Socket io init commit

--- frontend/src/Chat.js
+++ frontend/src/Chat.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Chat.css';
 import { Avatar, IconButton } from '@material-ui/core';
 import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
@@ -6,17 +6,41 @@ import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
 import MicIcon from "@material-ui/icons/Mic";
 import axios from './axios';
 
+import io from 'socket.io-client';
+
+const socket = io('localhost:3001');
+
 function Chat({ messages }) {
     const [input, setInput] = useState("")
+    const [isConnected, setIsConnected] = useState(socket.connected);
+    const [lastMessage, setLastMessage] = useState(null);
+
+    useEffect(() => {
+        socket.on('connect', () => {
+        setIsConnected(true);
+        });
+        socket.on('disconnect', () => {
+        setIsConnected(false);
+        });
+        socket.on('message', data => {
+        setLastMessage(data);
+        });
+        return () => {
+        socket.off('connect');
+        socket.off('disconnect');
+        socket.off('message');
+        };
+    });
 
     const sendMessage = (e) => {
         e.preventDefault();
 
+        socket.emit('newMessage', {message: input, name: "qwerty"});
         axios.post('/messages/new', {
             "message": input,
             "name": "qwerty",
             "timestamp": "some time",
-            "received": false
+            "received": true
         });
 
         setInput("");

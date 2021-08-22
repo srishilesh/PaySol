b151d1aca412d41c6c35fbad4f5aadc7574fa345
Harshak777 <Harshak777@users.noreply.github.com>
2021-08-22 11:23:13 +0530

    Added main chat pages

--- src/client/src/components/Chatscreen/chatArea.js
+++ src/client/src/components/Chatscreen/chatArea.js
@@ -1,15 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Pusher from 'pusher-js';
+import axios from './axios';
 import ConversationManager from './conversationManager';
 import ConversationScreen from './conversationScreen';
 import TransactionArea from '../TransactionScreen/transactionArea'
 
-import {Grid} from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 
 const Chatscreen = () => {
+
+    const [messages, setMessages] = useState([]);
+
+    useEffect(() => {
+        axios.get('/messages/sync')
+        .then(response => {
+        console.log(response);
+        setMessages(response.data);
+        });
+    }, [])
+
+    useEffect(() => {
+    const pusher = new Pusher('b2975d4dc0077112be09', {
+        cluster: 'ap2',
+        });
+
+        const channel = pusher.subscribe('messages');
+        channel.bind('inserted', function(newMessage) {
+        //alert(JSON.stringify(newMessage));
+        setMessages([...messages, newMessage]);
+        });
+
+        return () => {
+        channel.unbind_all();
+        channel.unsubscribe();
+        }
+
+    }, [messages]);
+
     return (
         <Grid container direction="row" justifyContent="flex-start" alignItems="flex-start">
             <ConversationManager />
-            <ConversationScreen />
+            <ConversationScreen messages={messages}/>
             <TransactionArea />
         </Grid>
     );
--- src/client/src/components/Chatscreen/conversationScreen.js
+++ src/client/src/components/Chatscreen/conversationScreen.js
@@ -1,11 +1,62 @@
-import React from 'react';
-import {Grid} from '@material-ui/core';
+import React, { useState } from 'react';
+import { Grid } from '@material-ui/core';
 import Container from '@material-ui/core/Container';
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from "@material-ui/icons/Mic";
+import axios from './axios';
+import './conversationScreen.css';
 
-const ConversationScreen = () => {
+const ConversationScreen = ({ messages }) => {
+
+    const [input, setInput] = useState("")
+
+    const sendMessage = (e) => {
+        e.preventDefault();
+
+        axios.post('/messages/new', {
+            "message": input,
+            "name": "qwerty",
+            "timestamp": "some time",
+            "received": true
+        });
+
+        setInput("");
+    };
+
     return (
         <Grid item xs={5} style={{backgroundColor: 'white'}}>
-           <Container>ConversationScreen</Container>
+           <Container>
+                <div className="chat_body">
+                    {messages.map((message) => (
+                        <p className={`chat_message ${message.received && "chat_reciever"}`}>
+                        <span className="chat_name">{message.name}</span>
+
+                            {message.message}
+
+                            <span className="chat_timestamp">
+                                {message.timestamp}
+                            </span>
+                    </p>
+                    ))}
+                    
+                </div>
+
+                <div className="chat_footer">
+                    <InsertEmoticonIcon />
+                    <form>
+                        <input
+                            value={input}
+                            onChange={e => setInput(e.target.value)}
+                            placeholder="Type a message"
+                            type="text"
+                        />
+                        <button onClick={sendMessage} type="submit">
+                            Send a message
+                        </button>
+                    </form>
+                    <MicIcon />
+                </div>
+           </Container>
         </Grid>
     );
 }
